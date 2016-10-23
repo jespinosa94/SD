4da3872d178eabf240fe82188bd4ccbdd861b1ce
sonip396 <jorge.espinosacam@gmail.com>
2016-10-23 18:35:16 +0200

    Conexion entre las 3 partes correcta, se muestran los valores de volumen del sensor 1

--- Controller/src/HiloController.java
+++ Controller/src/HiloController.java
@@ -28,15 +28,59 @@ public class HiloController extends Thread {
 		return p_Datos;
 	}
 	
-	public void escribeSocket(Socket p_sk, String p_datos) {
+	public void escribeSocket(Socket p_sk, String p_Datos) {
 		try {
 			OutputStream aux = p_sk.getOutputStream();
 			PrintWriter flujo = new PrintWriter(new OutputStreamWriter(aux));
-//d			flujo.println("{" + p_Datos + "}");
-			flujo.flush(); //limpiar buffer
+			flujo.println(p_Datos);
+			flujo.flush(); //Limpieza del Buffer
 		} catch(Exception e) {
-			System.out.println("Error" + e.toString());
+			System.out.println("Error: " + e.toString());
 		}
+	}
+	
+	public String leerArchivo(String s) {
+		String lectura = "";
+		File fichero = new File(System.getProperty("user.dir") + s); //Obtiene la ruta del fichero
+		try {
+			FileReader fr = new FileReader(fichero);
+			BufferedReader br = new BufferedReader(fr);
+			String linea;
+			while((linea = br.readLine()) != null) {
+				lectura += linea;
+			}
+			fr.close();
+			br.close();
+		} catch(Exception e) {
+			lectura = "Error 404: fichero no encontrado";
+		}
+		
+		return lectura;
+	}
+	
+	public String procesaPeticionRMI(InterfazSensor sensor, String servidor) {
+		String respuesta = "";
+		if(atributoSensor.contains("volumen")) {
+			System.setSecurityManager(new RMISecurityManager());
+			System.out.println("Controller pide el volumen " + "del sensor " + idSensor);
+				
+//d				System.out.println("[" + sensor.GetVolumen() + "]");
+			
+			String cuerpo = "";
+			cuerpo = leerArchivo("/index.html");
+			try {
+				if(cuerpo.contains("404")) {
+					System.out.println("Error al crear la página HTML del recurso pedido");
+				} else {
+					cuerpo+="<h1> Sensor " + idSensor + "</h1> </div>";
+					cuerpo+="<div class=\"color1 col-md-9\"> <h2> Volumen = " + sensor.GetVolumen() + "</h2> </div> </div> </div> </body> </html>";
+				}	
+			} catch(Exception e) {
+				System.out.println(e.toString());
+			}
+			respuesta = cuerpo;
+		}
+		return respuesta;
 	}
 	
 	@SuppressWarnings("deprecation")
@@ -62,22 +106,27 @@ public class HiloController extends Thread {
 			//Desencapsulación de la información recibida por HttpServer
 			atributoSensor = Cadena.split(" ")[0];
 			idSensor = Cadena.split(" ")[1];
-			System.out.println("Valores que recibe el controller: " + "[Atributo = " + atributoSensor + "], [id = " + idSensor + "]"); /* /controladorSD/volumen?Sonda=1 */			
-			if(atributoSensor.contains("volumen")) {
+			System.out.println("Valores que recibe el controller: " + "[Atributo = " + atributoSensor + "], [id = " + idSensor + "]"); /* /controladorSD/volumen?Sonda=1 */
+			
+			sensor = (InterfazSensor) Naming.lookup(servidor + idSensor);			
+			datosSocket = procesaPeticionRMI(sensor, servidor);
+			/*if(atributoSensor.contains("volumen")) {
 				System.setSecurityManager(new RMISecurityManager());
-				System.out.println("Pedido el volumen " + idSensor);
+				System.out.println("Controller pide el volumen " + "del sensor " + idSensor);
 				sensor = (InterfazSensor) Naming.lookup(servidor + idSensor);	
-				System.out.println("[[" + sensor.GetVolumen() + "]]");
+//d				System.out.println("[" + sensor.GetVolumen() + "]");
 				
 				String cuerpo = "";
-				cuerpo+="<hr> 	</hr> <h2> Sensor "+Cadena.split("\\?")[1].split("=")[1]+"</h2>";
-                cuerpo+="<a href=\"volumen?station="+Cadena.split("\\?")[1].split("=")[1]+"\" class=\"btn btn-danger\">volumen</a>";
-                cuerpo+="<a href=\"fecha?station="+Cadena.split("\\?")[1].split("=")[1]+"\" class=\"btn btn-primary\">fecha</a>";
-                cuerpo+="<a href=\"ultimafecha?station="+Cadena.split("\\?")[1].split("=")[1]+"\" class=\"btn btn-success\">ultimafecha</a>";
-                cuerpo+="<a href=\"luz?station="+Cadena.split("\\?")[1].split("=")[1]+"\" class=\"btn btn-warning\">luz</a>";
-                
-                datosSocket = cuerpo;
-			}
+				cuerpo = leerArchivo("/index.html");
+				if(cuerpo.contains("404")) {
+					System.out.println("Error al crear la página HTML del recurso pedido");
+				} else {
+					cuerpo+="<h1> Sensor " + idSensor + "</h1> </div>";
+					cuerpo+="<div class=\"color1 col-md-9\"> <h2> Volumen = " + sensor.GetVolumen() + "</h2> </div> </div> </div> </body> </html>";
+				}	
+				datosSocket = cuerpo;
+			}*/
+//d			System.out.print(datosSocket);
 			escribeSocket(skCliente, datosSocket);
 			skCliente.close();
 		} catch(Exception e) {
